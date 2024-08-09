c5b49deb192e7042809a298a109e85023bdaf763
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-08-09 19:59:25 +0200

    Add loop structures

--- semana03/script.js
+++ semana03/script.js
@@ -1,11 +1,11 @@
 let planetas = [];
 planetas.push('Mercurio', 'Venus', 'Terra', 'Marte', 'Jupiter');
 
-console.log('Tamanho do array: ' + planetas.length);
+//console.log('Tamanho do array: ' + planetas.length);
 
 planetas[5] = 'Zebes';
 
-console.log('Tamanho do array: ' + planetas.length);
+//console.log('Tamanho do array: ' + planetas.length);
 
 planetas.push('Urano');
 
@@ -15,8 +15,27 @@ planetas.push('Netuno');
 
 planetas.push('Plutão');
 
-console.log('Planetas: ' + planetas + '!');
+//console.log('Planetas: ' + planetas + '!');
 
 planetas.pop();
 
-console.log('Planetas: ' + planetas + '!');
+//console.log('Planetas: ' + planetas + '!');
+
+//loops for
+for(let i = 0; i < 5; i++) {
+    console.log(i + 1);
+}
+
+//while com o mesmo código de for
+let i = 0;
+while(i < 5) {
+    console.log(i + 1);
+    i++;
+}
+
+//do while
+i =0; //zerando a variável
+do {
+    alert(i + 1);
+    i++;
+} while(i < 5);
