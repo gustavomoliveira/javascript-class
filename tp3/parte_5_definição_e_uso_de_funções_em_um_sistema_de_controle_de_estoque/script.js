5938da5f9fc7d06df768060519b45c4a8f997396
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-09-04 19:46:42 +0200

    Add a storage control system created  with JS

---
+++ tp3/parte_5_definição_e_uso_de_funções_em_um_sistema_de_controle_de_estoque/script.js
@@ -0,0 +1,71 @@
+/* 
+Exercício 12 Grupo: Funções de adição e atualização de produtos
+Requisito:
+    Crie um array chamado estoque que armazenará os produtos da loja.
+    Cada produto deve ser representado como um objeto com as propriedades: nome, quantidade, precoUnitario.
+    Implemente uma função adicionarProduto que permita adicionar um novo produto ao estoque.
+    A função deve receber os parâmetros nome, quantidade e precoUnitario.
+    Implemente uma função atualizarProduto que atualize a quantidade de um produto existente no estoque com base no nome.
+*/
+
+let estoque = [];
+
+function adicionarProduto(nome, quantidade, precoUnitario) {
+    let produto = {
+    nome: nome,
+    quantidade: quantidade,
+    precoUnitario: precoUnitario 
+    };
+    estoque.push(produto)
+}
+
+adicionarProduto('Celular', 1, 2000);
+adicionarProduto('Garrafa', 1, 5.50);
+adicionarProduto('Camisa', 1, 120);
+console.log(estoque)
+
+
+function atualizarProduto(oNome, aQuantidade) {
+    for(let i = 0; i < estoque.length; i++) {
+        if(estoque[i].nome === oNome) //acessa o nome através do loop e atribui a nova quantidade.
+            estoque[i].quantidade = aQuantidade;
+            return;
+    }
+    alert('Produto não encontrado. Tente novamente.');
+}
+
+atualizarProduto('Celular', 4);
+console.log(estoque);
+
+/* 
+Exercício 13 Grupo: Removendo produtos, calculando o valor total do estoque e relatório de estoque
+Requisito:
+    Crie uma função removerProduto que remova um produto do estoque com base no nome.
+    Implemente uma função calcularValorTotal que retorne o valor total de todos os produtos no estoque.
+    Desenvolva uma função relatorioEstoque que exiba um relatório com todos os produtos, suas quantidades
+    e valores totais individuais (quantidade * preço unitário).
+*/
+
+function removerProduto(nome) {
+    return estoque.filter(item => item.nome !== nome);
+}
+
+let estoqueFiltrado = removerProduto('Garrafa');
+console.log(estoqueFiltrado);
+
+function calcularValorTotal() {
+    return estoque.reduce((total, item) => total + (item.quantidade * item.precoUnitario), 0); //o acumulador é somado ao produto de cada qtd * preco.
+}
+
+let valorTotal = calcularValorTotal();
+console.log(`Valor total do estoque: R$ ${calcularValorTotal()}`);
+
+function relatorioEstoque() {
+    for(let i = 0; i < estoque.length; i++) {
+        console.log(`Produtos em estoque: ${estoque[i].nome}\n
+com estoque de ${estoque[i].quantidade} unidades\n
+valor total individual de R$${estoque[i].quantidade * estoque[i].precoUnitario}`)
+    }
+}
+
+relatorioEstoque();
---
+++ tp3/parte_6_sistema_de_gerenciamento_de_notas_de_estudantes/script.js
@@ -0,0 +1,46 @@
+/* 
+Exercício 14 Grupo: Funções para cálculo de médias, situação do
+estudante e função recorrente para identificação de recuperação
+Requisito:
+    Crie um array chamado estudantes para armazenar os dados dos alunos.
+    Cada estudante deve ser representado como um objeto com as propriedades:
+    nome, notas (um array de números), e situacao (inicialmente vazio).
+    Crie uma função calcularMedia que receba um array de notas e retorne a média.
+    Crie uma função atualizarSituacao que, utilizando a média, atualize a situação
+    do estudante para "Aprovado" ou "Recuperação".
+    Implemente uma função recorrente verificarRecuperacao que receba a lista de estudantes e,
+    de forma recorrente, encontre todos que estão em recuperação, devolvendo uma lista separada.
+    Crie uma função gerarRelatorio que exiba um relatório final com o nome, notas, média e situação de cada estudante.
+    Refatore o código para garantir que todas as funções estão bem organizadas e o código está limpo e legível.
+Uso de funções de alta ordem:
+    Utilize métodos como map para aplicar as funções de cálculo e atualização a todos os estudantes.
+*/
+
+let estudantes = [{nome: 'João', notas: [6, 7, 5, 8, 10], situacao: ''},
+{nome: 'Marcela', notas: [8, 7, 9, 6, 4], situacao: ''},
+{nome: 'Carlos', notas: [9, 9, 7, 9, 6], situacao: ''}];
+
+function calcularMedia(estudante) {
+    return estudante.notas.reduce((total, nota) => total + (nota / estudante.notas.length), 0);
+}
+
+let medias = [];
+
+estudantes.forEach(estudantes => {
+    let media = calcularMedia(estudantes);
+    medias.push(media);
+    console.log(`A média de ${estudantes.nome} é ${media}.`);
+});
+
+
+function atualizarSituacao(media) {
+    if(media >= 7) return 'Aprovado';
+    if(media >= 5 && media < 7) return 'Recuperação'
+}
+
+let situacao = atualizarSituacao(medias);
+console.log(situacao);
+
+function verificarRecuperacao(estudantes) {
+    estudantes.map(nome => estudantes.nome );
+}
