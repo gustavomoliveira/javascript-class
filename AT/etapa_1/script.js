5edae1df5ab5ba08f516ef983f8ff506031f0412
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-09-18 21:37:23 +0200

    Add exibirPedidos function to create a report

--- AT/etapa_1/script.js
+++ AT/etapa_1/script.js
@@ -9,29 +9,21 @@ e usar arrays para armazenar os objetos criados.
 O array externo será usado para armazenar todos os objetos criados a partir da classe principal
 do projeto (como "Pedido" no exemplo do professor). Este array será a coleção externa onde todos
 os pedidos criados serão armazenados e gerenciados.
-*/
 
-/* 
 2 - Função de inclusão de pedidos na coleção externa:
 Dentro da classe Pedido, crie uma função que permita adicionar novos pedidos à coleção externa.
 Essa função receberá as informações do pedido, do solicitante e dos produtos associados a cada
 pedido e deverá inserir o pedido devidamente preenchido no array externo.
-*/
 
-/* 
 3 - Relacionamentos entre classes:
 Para que um pedido seja incluído na coleção externa, é necessário que ele esteja relacionado
 com duas outras classes associadas: Produtos (vários produtos por pedido), Solicitante (um solicitante por pedido).
 A função de inclusão de pedidos também será responsável por associar o objeto Solicitante a cada pedido.
-*/
 
-/* 
 4 - Função para adicionar produtos ao pedido:
 Dentro da classe Pedido, crie uma função para adicionar um produto por vez na coleção de produtos já existentes no pedido.
 O array de produtos ficará dentro de cada pedido e será alimentado conforme novos produtos forem incluídos.
-*/
 
-/* 
 5 - Criação do Solicitante:
 Quando um pedido for criado e inserido na coleção externa, as informações do solicitante (nome, e-mail etc) deverão
 ser passadas como parâmetros, e um novo objeto solicitante será instanciado e associado ao pedido automaticamente
@@ -71,9 +63,11 @@ class Pedido {
     }
 }
 
+//criando dois produtos
 const produto1 = new Produtos('Celular', 2000, 2);
 const produto2 = new Produtos('Carregador', 120, 4);
 
+//criando um pedido
 let pedido = new Pedido();
 pedido.adicionarProduto(produto1);
 pedido.adicionarProduto(produto2);
@@ -81,6 +75,27 @@ pedido.adicionarPedidos('Gustavo', 'gustavo@email.com');
 
 console.log(pedidosArmazenados);
 
+/* 
+Etapa 2 Trabalhando com Repetições/Interações
+Na segunda etapa do projeto, o foco será programar repetições/interações em JavaScript,
+aplicando o uso de loops para percorrer, inserir e manipular dados, além de estruturar a
+interação entre o usuário e o sistema para realizar a entrada de dados e a navegação por meio de loops aninhados.
+
+1 - Função externa para exibir todos os pedidos cadastrados:
+Crie uma função fora da classe que exiba todos os pedidos armazenados na coleção externa (array de pedidos).
+Essa função deve percorrer a coleção de pedidos usando uma estrutura de repetição e exibir pelo menos um atributo
+de cada classe relacionada (Pedido, Solicitante e Produto).
+*/
+
+function exibirPedidos(pedidos) {
+    pedidos.forEach(elemento => {
+         console.log(`Nome do Solicitante: ${elemento.solicitante.nome}, 
+Nomes dos Produtos: ${elemento.produtos.forEach(el => el.nome)},
+Id do Pedido: ${elemento.id}`);
+    })
+}
+
+exibirPedidos(pedidosArmazenados);
 
 
 
