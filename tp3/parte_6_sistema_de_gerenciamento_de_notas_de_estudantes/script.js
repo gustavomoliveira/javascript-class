4424a9305c97d0daa5e67dfc84c161efe68c7639
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-09-19 15:03:08 +0200

    Add gerarRelatorio function and updated the verificarRecuperacao function

--- tp3/parte_6_sistema_de_gerenciamento_de_notas_de_estudantes/script.js
+++ tp3/parte_6_sistema_de_gerenciamento_de_notas_de_estudantes/script.js
@@ -2,7 +2,6 @@
 Exercício 14 Grupo: Funções para cálculo de médias, situação do
 estudante e função recorrente para identificação de recuperação
 Requisito:
-    Crie uma função gerarRelatorio que exiba um relatório final com o nome, notas, média e situação de cada estudante.
     Refatore o código para garantir que todas as funções estão bem organizadas e o código está limpo e legível.
 Uso de funções de alta ordem:
     Utilize métodos como map para aplicar as funções de cálculo e atualização a todos os estudantes.
@@ -14,12 +13,13 @@ nome, notas (um array de números), e situacao (inicialmente vazio). */
 let estudantes = [
     { nome: 'João', notas: [6, 7, 5, 8, 10], situacao: '' },
     { nome: 'Marcela', notas: [6, 7, 0, 6, 4], situacao: '' },
-    { nome: 'Carlos', notas: [9, 9, 7, 9, 6], situacao: '' }
+    { nome: 'Carlos', notas: [9, 9, 7, 9, 6], situacao: '' },
+    { nome: 'Marcos', notas: [4, 2, 5, 0, 6], situacao: '' }
 ];
 
 /* Crie uma função calcularMedia que receba um array de notas e retorne a média. */
 function calcularMedia(estudante) {
-    return estudante.notas.reduce((total, nota) => total + (nota / estudante.notas.length), 0);
+    return parseFloat(estudante.notas.reduce((total, nota) => total + (nota / estudante.notas.length), 0).toFixed(2));
 }
 let medias = estudantes.map(estudante => calcularMedia(estudante));
 console.log(medias);
@@ -41,6 +41,29 @@ console.log(estudantes);
   
 /* Implemente uma função recorrente verificarRecuperacao que receba a lista de estudantes e,
 de forma recorrente, encontre todos que estão em recuperação, devolvendo uma lista separada. */
+
+//ELBERTH COMENTOU EM SALA QUE IRIA REVISAR ESSA QUESTÃO JÁ QUE A RECURSIVIDADE FICOU BASTANTE CONFUSA.
 function verificarRecuperacao(estudantes) {
-    
+    return estudantes.filter(estudante => estudante.situacao === 'Recuperação');
 }
+
+let estudantesRecuperacao = verificarRecuperacao(estudantes);
+console.log(estudantesRecuperacao);
+
+/* 
+Crie uma função gerarRelatorio que exiba um relatório final com o nome, notas, média e situação de cada estudante.
+*/
+function gerarRelatorio(estudantes, medias) {
+    let imprime = '';
+    estudantes.forEach((estudante, i) => {
+    imprime += `Estudante: ${estudante.nome}\n`;
+        estudante.notas.forEach((nota, i) => {
+            imprime += `Nota ${i + 1}: ${nota}\n`
+        });
+    imprime += `Média Final: ${medias[i]}\n`;
+    imprime += `Situação: ${estudante.situacao}\n`;
+    });
+    console.log(imprime);
+}
+
+gerarRelatorio(estudantes, medias);
