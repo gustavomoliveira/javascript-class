feb3a5f9a5f691cb4a7e73c08a63525f122b33be
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-09-27 08:55:43 +0200

    Changed some functions and outputs to make the whole code more readable

--- AT/script.js
+++ AT/script.js
@@ -1,9 +1,8 @@
 /* Sistema de Gestão de Oficina
-
 Classes:
-Serviço: cliente, carros.
-Carro: marca, modelo, ano.
-Cliente: nome, email.
+Serviço: cliente, id, carros.
+Carro: marca, modelo, ano, preçoServico.
+Cliente: nome, e-mail.
 */
 
 let garagemOficina = [];
@@ -19,14 +18,14 @@ class Cliente {
 }
 
 class Carro {
-    constructor(marca, modelo, ano, preco) {
+    constructor(marca, modelo, ano, precoServico) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
-        this.preco = preco;
+        this.precoServico = precoServico;
     }
     toString() {
-        return `${this.marca.toUpperCase()} - ${this.modelo.toUpperCase()} - ${this.ano} - Valor do serviço: R$${Math.round(this.preco).toFixed(2)}`
+        return `${this.marca.toUpperCase()} - ${this.modelo.toUpperCase()} - ${this.ano} - Valor do serviço: R$${Math.round(this.precoServico).toFixed(2)}`
     }
 }
 
@@ -40,15 +39,19 @@ class Servico {
         let todosOsCarros = this.carros.map(carro => carro.toString()).join('\n');
         return `Serviço[${this.id}] - Cliente: ${this.cliente.toString()}\nCarros:\n${todosOsCarros}`;
     }
+    //Função de adição de Serviço ao array geral garagemOficina
     adicionarServico() {
         garagemOficina.push(this);
     }
+    //Função de adição do objeto Carro ao array carros
     adicionarCarro(carro) {
         this.carros.push(carro);
     }
+    //Função de remoção de carro por filtro de modelo
     removerModeloCarro(modelo) {
         this.carros = this.carros.filter(carro => carro.modelo.toLowerCase() !== modelo.toLowerCase());
     }
+    //Função de modificação de atributo e valor do mesmo baseado nos parâmetros passados
     modificarAtributo(atributo, valor) {
         if(this.hasOwnProperty(atributo)) {
             this[atributo] = valor;
@@ -59,24 +62,32 @@ class Servico {
     }
 }
 
-//criando carros
-const carro1 = new Carro('Mercedes Benz', 'GLC', 2023, 5200);
+
+
+/* DADOS PARA TESTE: ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+Carros, Clientes, Métodos, Serviços preenchidos e pre-definidos */
+
+/* const carro1 = new Carro('Mercedes Benz', 'GLC', 2023, 5200);
 const carro2 = new Carro('BMW', 'X5', 2020, 5100);
 const carro3 = new Carro('VW', 'Fusca', 1968, 2400);
 const carro4 = new Carro('Fiat', 'Uno', 2014, 1200);
-const carro5 = new Carro('Ferrari', 'Enzo', 2016, 14000);
+const carro5 = new Carro('Ferrari', 'Enzo', 2016, 24000);
+const carro6 = new Carro('Tesla', 'EZD', 2022, 7350);
 
-//criando cliente
-const cliente1 = new Cliente('gustavo', 'gustavo@email.com');
-const cliente2 = new Cliente('bartô', 'barto@email.com');
-const cliente3 = new Cliente('joão', 'joao@email.com');
 
-//criando serviço
+const cliente1 = new Cliente('joão', 'joao@email.com');
+const cliente2 = new Cliente('maria', 'maria@email.com');
+const cliente3 = new Cliente('jose', 'jose@email.com');
+const cliente4 = new Cliente('fernando', 'fernando@email.com');
+const cliente5 = new Cliente('claudio', 'claudio@email.com');
+
+
 let servico = new Servico(cliente1, 123);
 servico.adicionarCarro(carro2);
 servico.adicionarCarro(carro3);
+servico.adicionarCarro(carro6);
 servico.adicionarServico();
-servico.removerModeloCarro('x5'); //recebe o modelo do carro para remoção.
+servico.removerModeloCarro('x5');
 servico.modificarAtributo('id', 125);
 
 let servico2 = new Servico(cliente2, 654);
@@ -91,8 +102,46 @@ servico3.adicionarCarro(carro3);
 servico3.adicionarServico();
 
 console.log(garagemOficina);
-console.log(servico.carros);
+console.log(servico.carros); */
 
+//Função para cadastro de Serviço usando loop dentro de loop
+function cadastroServico() {
+    let novoServico = true;
+    while(novoServico) {
+        //Coleta dados do cliente
+        const nomeCliente = prompt('Insira o nome do cliente:');
+        const emailCliente = prompt('Insira o e-mail de contato do cliente:');
+        const cliente = new Cliente(nomeCliente, emailCliente);
+
+        //Coleta dados do serviço
+        const idServico = parseInt(prompt('Insira o ID do serviço a ser realizado:'));
+        const servico = new Servico(cliente, idServico);
+
+        let novoCarro = true;
+        while(novoCarro) {
+            //Coleta dados do carrogustavo
+            const marcaCarro = prompt('Insira a marca do carro:');
+            const modeloCarro = prompt('Insira o modelo do carro:');
+            const anoCarro = parseInt(prompt('Insira o ano de fabricação do carro:'));
+            const precoServico = parseFloat(prompt('Insira o preço do serviço a ser realizado:'));
+            const carro = new Carro(marcaCarro, modeloCarro, anoCarro, precoServico);
+
+            //Adiciona carro ao serviço
+            servico.adicionarCarro(carro);
+            //Pergunta ao usuário se ele deseja adicionar mais carros ao serviço
+            novoCarro = confirm('Deseja adicionar mais carros ao serviço? "Ok" para "Sim" / "Cancelar" para "não".');
+        }
+        //Adiciona o serviço criado à garagem
+        servico.adicionarServico();
+        //Pergunta ao usuário se ele deseja adicionar um novo serviço
+        novoServico = confirm('Deseja adicionar um novo serviço? "Ok" para "Sim" / "Cancelar" para "não".');
+    }
+    console.log(garagemOficina);
+}
+
+cadastroServico();
+
+//Exibe relatório completo contendo toda a Etapa 4 - Ex. 1, 2 e 3 com manipulação de string
 function exibirServicos() {
     let imprime = '';
     garagemOficina.forEach(servico => {
@@ -101,89 +150,37 @@ function exibirServicos() {
     console.log(imprime);
 }
 
-/*function cadastroServico() {
-    let novoServico = true;
-    while(novoServico) {
-        //coleta dados do cliente
-        const nomeCliente = prompt('Insira o nome do cliente:');
-        const emailCliente = prompt('Insira o e-mail de contato do cliente:');
-        const cliente = new Cliente(nomeCliente, emailCliente);
-
-        //coleta dados do serviço
-        const idServico = parseInt(prompt('Insira o ID do serviço a ser realizado:'));
-        const precoServico = parseFloat(prompt('Insira o preço do serviço a ser realizado:'));
-        const servico = new Servico(cliente, idServico, precoServico);
-
-        let novoCarro = true;
-        while(novoCarro) {
-            //coleta dados do carro
-            const marcaCarro = prompt('Insira a marca do carro:');
-            const modeloCarro = prompt('Insira o modelo do carro:');
-            const anoCarro = parseInt(prompt('Insira o ano de fabricação do carro:'));
-            const carro = new Carro(marcaCarro, modeloCarro, anoCarro);
-
-            //adiciona carro ao serviço
-            servico.adicionarCarro(carro);
-            //pergunta ao usuário se ele deseja adicionar mais carros ao serviço
-            novoCarro = confirm('Deseja adicionar mais carros ao serviço? "Ok" para "Sim" / "Cancelar" para "não".');
-        }
-        //adiciona o serviço criado à garagem
-        servico.adicionarServico();
-        //pergunta ao usuário se ele deseja adicionar um novo serviço
-        novoServico = confirm('Deseja adicionar um novo serviço? "Ok" para "Sim" / "Cancelar" para "não".');
-    }
-    console.log(garagemOficina);
-}
-
-cadastroServico(); */
-
-//exibe a garagem apenas após o cadastro.
-exibirServicos();
-
+//Função totalizadora para calcular duas propriedades
 const totalizadores = (garagem) => {
     const osTotalizadores = garagem.map(servico => {
-        const valorTotalServico = servico.carros.reduce((total, carro) => total + carro.preco, 0);
+        const valorTotalServico = servico.carros.reduce((total, carro) => total + carro.precoServico, 0);
         const totalCarros = servico.carros.length;
         return {
             valorTotalServico: `R$${Math.round(valorTotalServico).toFixed(2)}`,
             totalCarros: totalCarros
         };
     });
-   console.log(`Valores totais de preço por serviço e número de carros: ${JSON.stringify(osTotalizadores, null, 2)}`);
+   console.log(`Valores totais de preço e número de carros: ${JSON.stringify(osTotalizadores, null, 2)}`);
 }
-totalizadores(garagemOficina);
+totalizadores(garagemOficina); //parâmetro já passado para teste
 
-//map em todo o array da garagem, acessa os serviços, dentro dos serviços a coleção de carros que é aberta em um único array e filtrada por ano acima de 2010
+//Função para filtro de propriedade
 const anoFabricacao = (garagem, ano) => {
    const oAnoDeFabricacao = garagem.map(servico => servico.carros).flat().filter(carro => carro.ano >= ano);
    console.log(`Carros selecionados a partir do ano de ${ano}: ${JSON.stringify(oAnoDeFabricacao, null, 2)}`);
 }
-anoFabricacao(garagemOficina, 2010);
+anoFabricacao(garagemOficina, 2010); //parâmetro já passado para teste
 
-//usando some() para verificar a ocorrência de preço
-const ocorrenciaPreco = (garagem, preco) => {
+//Função para verificação de ocorrência
+const ocorrenciaPreco = (garagem, precoServico) => {
     const ocorrencia = garagem.some(servico => {
-        return servico.carros.some(carro => carro.preco > preco);
+        return servico.carros.some(carro => carro.precoServico > precoServico);
     });
-    
-    console.log(ocorrencia ? `Existe serviço a partir de R$${Math.round(preco).toFixed(2)}` : `Não existe serviço a partir de ${Math.round(preco).toFixed(2)}`);
+    console.log(ocorrencia ? `Existe serviço a partir de R$${Math.round(precoServico).toFixed(2)}` : `Não existe serviço a partir de R$${Math.round(precoServico).toFixed(2)}`);
 }
-ocorrenciaPreco(garagemOficina, 5000);
+ocorrenciaPreco(garagemOficina, 5000); //parâmetro já passado para teste
 
-//etapa 4 - ex. 1, 2 e 3 - função de manipulação de string
-/* function relatorioDetalhado(servicos) {
-   let imprime = '';
+//Chama relatório completo ao fim de todos os dados
+exibirServicos();
 
-    servicos.forEach((servico, i) => {
-        imprime += `\nRelatório completo de todos os dados do Serviço ${i + 1}, de ID ${servico.id} e valor total a ser pago de R$${Math.round(servico.preco).toFixed(2)}:\n
-Cliente ${servico.cliente.nome.charAt(0).toUpperCase() + servico.cliente.nome.slice(1)}, de e-mail de contato: ${servico.cliente.email.trim()},\n
-Possui ${servico.carros.length} carro(s) em nossa garagem para realização de manutenção ou reparos. As especificações do(s) veículo(s):\n`;
-        servico.carros.forEach((carro, j) => {
-            imprime += `\nCarro ${j + 1} de fabricante: ${carro.marca.toUpperCase()}, modelo: ${carro.modelo.toUpperCase()} e ano de fabricação ${carro.ano}.\n`;
-        });
-    });
-    console.log(imprime);
-}
-
-relatorioDetalhado(garagemOficina); */
 
